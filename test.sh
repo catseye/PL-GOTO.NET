f5ac557e1b19f38f2fc3ffe4d04db68f2e7f4320
catseye <cpressey@gmail.com>
2011-12-10 16:58:49 -0600

    Update tests for upcoming Falderal version 0.5.

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# This assumes Falderal 0.4 is installed.  The easiest way to do this is to
-# install it as a Cabal package:
+# This assumes Falderal 0.4 is installed.  The easiest way to do this
+# currently is to install it as a local Cabal package:
 #   $ hg clone https://bitbucket.org/catseye/falderal
 #   $ cd falderal
-#   $ cabal install --prefix=$HOME --user
+#   $ ./install.sh
 
-falderal test standard PLexceptGOTOdotNET.falderal
+falderal test PLexceptGOTOdotNET.falderal
