65f05d6ff2c1486fbd3f7d9dbba598781770e001
Chris Pressey <cpressey@catseye.tc>
2014-08-17 12:59:46 +0100

    Allow 2/3 tests to still pass even if ilasm isn't installed.
    --HG--
    rename : tests/PLexceptGOTOdotNET.markdown => tests/PLexceptGOTOdotNET-run.markdown

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-./build.sh
-falderal test tests/PLexceptGOTOdotNET.markdown
+echo "Building..."
+./build.sh || exit 1
+echo "Testing frontend and interpreter..."
+falderal test tests/PLexceptGOTOdotNET.markdown || exit 1
+if [ `which ilasm`x = x ]; then
+    echo "ilasm not found, skipping compiler tests."
+else
+    echo "Testing compiler..."
+    falderal test tests/PLexceptGOTOdotNET-run.markdown || exit 1
+fi
