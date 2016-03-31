8ace37499f5a228b8aded5993463ff0e598e4e36
Chris Pressey <cpressey@catseye.tc>
2016-03-31 15:24:07 +0100

    Remove deprecated `test` argument when running `falderal`.

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 echo "Building..."
 ./build.sh || exit 1
 echo "Testing frontend and interpreter..."
-falderal test tests/PLexceptGOTOdotNET.markdown || exit 1
+falderal tests/PLexceptGOTOdotNET.markdown || exit 1
 if [ `which ilasm`x = x ]; then
     echo "ilasm not found, skipping compiler tests."
 else
     echo "Testing compiler..."
-    falderal test tests/PLexceptGOTOdotNET-run.markdown || exit 1
+    falderal tests/PLexceptGOTOdotNET-run.markdown || exit 1
 fi
