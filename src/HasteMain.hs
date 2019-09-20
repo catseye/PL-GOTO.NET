d15b2549d5bfacc52af5c35eae938a115402552e
Chris Pressey <cpressey@catseye.tc>
2019-09-20 16:54:14 +0100

    Simplify a little more, even.

--- src/HasteMain.hs
+++ src/HasteMain.hs
@@ -4,11 +4,11 @@ import Haste
 import Haste.DOM (withElems, getValue, setProp)
 import Haste.Events (onEvent, MouseEvent(Click))
 
-import PLexceptGOTOdotNET
+import PLexceptGOTOdotNET (run)
 
 main = withElems ["prog", "result", "run-button"] driver
 
-driver [progElem, resultElem, runButtonElem] = do
+driver [progElem, resultElem, runButtonElem] =
     onEvent runButtonElem Click $ \_ -> do
         Just prog <- getValue progElem
         setProp resultElem "textContent" $ run prog
